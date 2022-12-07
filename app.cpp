b021dea752f8341010ecdf3d5c088fb91cf4f92f
Camdyn <camdyn3@protonmail.com>
2022-12-07 15:22:31 -0800

    add function for idx - mean

--- app.cpp
+++ app.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 double calculateMean(int popSize, double *pop)
 {
+    // used to calculate xbar and ybar
     double sum = 0;
     double mean = 0;
     for (int idx = 0; idx < popSize; idx++)
@@ -16,6 +17,16 @@ double calculateMean(int popSize, double *pop)
     return mean;
 }
 
+void calculateIndexMinusMean(int popSize, double mean, double *pop, double *results)
+{
+    // used to calculate xi - xbar and yi - ybar
+    // apparently I can't return a pointer, so I'll assign the values to the new function directly in here
+    for (int idx = 0; idx < popSize; idx++)
+    {
+        results[idx] = (pop[idx] - mean);
+    }
+}
+
 double linearRegression()
 {
     // holders for important values
@@ -24,6 +35,9 @@ double linearRegression()
 
     int popSize;
     double *xValues, *yValues;
+    double *xiMinusXBar, *yiMinusYBar;
+
+    double *xiMinusXBarSquared, *yiMinusXBarSquared;
 
     cout << "How many subjects are in your population?: ";
     cin >> popSize;
@@ -52,6 +66,20 @@ double linearRegression()
 
     cout << "xbar = " << xBar << '\t'
          << "ybar = " << yBar << endl;
+
+    // creating arrays for xi-xbar and yi-ybar
+    xiMinusXBar = new double[popSize];
+    yiMinusYBar = new double[popSize];
+
+    // calculating values for xi-xbar and yi-ybar
+    calculateIndexMinusMean(popSize, xBar, xValues, xiMinusXBar);
+    calculateIndexMinusMean(popSize, yBar, yValues, yiMinusYBar);
+
+    // creating arrays for (xi - xbar)^2 and (yi - ybar)^2
+    xiMinusXBarSquared = new double[popSize];
+    yiMinusXBarSquared = new double[popSize];
+
+    // calculating values for (xi - xbar)^2 and (yi-ybar)^2
 
     return 0;
 }
